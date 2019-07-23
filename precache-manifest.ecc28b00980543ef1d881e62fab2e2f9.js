ecc4b6cee64dfb24c690d622ed0bf353a168c598
Katharine Cooney <katharinecooney@gmail.com>
2019-07-23 15:55:33 -0500

    Updates

---
+++ precache-manifest.ecc28b00980543ef1d881e62fab2e2f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535d4bd22b1a4b8f97488e5f000c4db7",
+    "url": "/fact-calendar/index.html"
+  },
+  {
+    "revision": "31274734ab3ce6f9fe64",
+    "url": "/fact-calendar/static/css/main.2f49e687.chunk.css"
+  },
+  {
+    "revision": "e67b266a2755ea3ef27a",
+    "url": "/fact-calendar/static/js/2.38991e27.chunk.js"
+  },
+  {
+    "revision": "31274734ab3ce6f9fe64",
+    "url": "/fact-calendar/static/js/main.edb7f46a.chunk.js"
+  },
+  {
+    "revision": "770435c7951cacb1f471",
+    "url": "/fact-calendar/static/js/runtime~main.223b6ad7.js"
+  }
+]);
